ab5f54b4592c5e44d5ef4d777a6b790e805518b2
Vinc-78 <vincenzodicostanzo.um@gmail.com>
2022-01-12 15:19:08 +0100

    add migration, model , table

---
+++ resources/views/comics/home.blade.php
@@ -0,0 +1,27 @@
+@extends('comics.layout')
+
+@section('title', "Home" )
+    
+@section('content')
+
+<div class="container">
+
+    <h1>Lista utenti</h1>
+  
+    <a class="btn btn-link" href=" ">Aggiungi...</a>  {{-- {{ route('users.create') }} --}}
+  
+    <br>
+    <ul>
+      @foreach($comics as $comic)
+        <li><a href="">{{ $comic["title"] }}</a></li>
+
+       {{--  {{ route('comics.create') }} --}}
+
+
+      @endforeach
+    </ul>
+  </div>
+
+
+    
+@endsection
